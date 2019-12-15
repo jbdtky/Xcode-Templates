5785a165fa8e1175990d3d291cd0cbbf77e4086f
jbdtky <jb@bitcoin.com>
2019-12-15 12:54:30 +0900

    Upgrade the MVVM template

--- ViewController File.xctemplate/___FILEBASENAME___.swift
+++ ViewController File.xctemplate/___FILEBASENAME___.swift
@@ -1,11 +1,15 @@
 //___FILEHEADER___
 
 import UIKit
+import Combine
 
 class ___FILEBASENAME___: UIViewController {
     
     private var disposables = Set<AnyCancellable>()
-    private let viewModel = ViewModel(repository: RepositoryImpl())
+    lazy var viewModel: ViewModel = {
+        // TODO: Choose your way to initialize it depending on Storyboard or Code
+        fatalError("It should be initialized")
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +23,20 @@ class ___FILEBASENAME___: UIViewController {
     }
     
     func setupViewModel() {
+        // TODO: Setup ViewModel
         viewModel
             .state
             .receive(on: DispatchQueue.main)
             .sink { state in
                 switch state {
                 case .initial:
-                    // ...
+                    // TODO: Setup the UI with the fetched data
+                    break
+                case .success:
+                    // TODO: It may want to transit?
                     break
                 }
             }
-        .store(in: &disposables)
+            .store(in: &disposables)
     }
 }
--- ViewModel File.xctemplate/___FILEBASENAME___.swift
+++ ViewModel File.xctemplate/___FILEBASENAME___.swift
@@ -12,29 +12,35 @@ class ___FILEBASENAME___ {
     private var disposables = Set<AnyCancellable>()
     private let repository: Repository
     
-    private(set) var state: CurrentValueSubject<State, Error> // If state is needed
-    private(set) var value: CurrentValueSubject<String, Error> // If value is needed
+    private(set) var state: PassthroughSubject<State, Never>()
+    private(set) var strongValue: String
     
-    init(repository: Repository) {
+    init(repository: Repository = RepositoryImpl(),
+         value: String) {
         self.repository = repository
-        state = CurrentValueSubject(.initial) // If state is needed
+        self.weakValue = CurrentValueSubject(nil) // Placeholder
+        self.strongValue = value
+        
+        self.repository
+            .get(arg)
+            .sink { [weak self] value in
+                self?.strongValue = value
+                self?.state.send(.initial)
+            }
+            .store(in: &disposables)
     }
     
-    func fetch(_ arg: String) {
-            repository
-                .get(arg)
-                .receive(on: DispatchQueue.main)
-                .sink { [weak self] value in
-                    self?.value.value = value
-                }
-                .store(in: &disposables)
+    func submitValue() {
+        repository
+            .post()
+            .sink { [weak self] _ in
+                self?.state.value = .success
+            }
+            .store(in: &disposables)
     }
 }
 
 protocol Repository {
     func get(_ arg: String) -> AnyPublisher<String, Never>
-}
-
-protocol Service {
-    func fetch(_ arg: String) -> AnyPublisher<String, Never>
+    func post() -> AnyPublisher<String, Never>
 }
